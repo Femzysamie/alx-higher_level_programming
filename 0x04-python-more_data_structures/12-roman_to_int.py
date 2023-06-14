06bd01e1bfcc114d691e302460e9e1be440a32a5
Femzysamie <femzysamiejebty@gmail.com>
2023-06-14 15:09:43 -0700

    task 11 corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
     roman_letters = [
-        ['I', 1], ['V', 5], ['X', 10], ['L', 50],
-        ['C', 100], ['D', 500], ['M', 1000]
+        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+        ['X', 10], ['V', 5], ['I', 1]
     ]
     num = 0
     last = 0

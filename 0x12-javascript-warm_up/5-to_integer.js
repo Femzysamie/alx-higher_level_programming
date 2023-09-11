ef13721b3dd97624874adf10a7de94917f902352
Femzysamie <femzysamiejebty@gmail.com>
2023-09-11 20:18:48 +0000

    corrected

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,8 @@
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+#!/usr/bin/node
+const argv = process.argv;
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
